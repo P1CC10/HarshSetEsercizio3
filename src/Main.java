24f3250631a98a803ffebe73a4ff3d53af0dadba
Ivan <ivanpiccioni.work@gmail.com>
2023-12-15 11:48:46 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        // Chiamo la funzione per eseguire l'operazione
+
         HashSet<Integer> hashSet = creaHashSetRiempito();
         verificaElementoEdElimina(hashSet);
         svuotaHashSetEVerifica(hashSet);
     }
 
-    // Funzione che restituisce un HashSet riempito
+
     public static HashSet<Integer> creaHashSetRiempito() {
         HashSet<Integer> hashSet = new HashSet<>();
 
-        // Aggiungi elementi all'HashSet
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
@@ -21,20 +20,14 @@ public class Main {
         return hashSet;
     }
 
-    // Funzione che verifica ogni elemento dell'HashSet e lo elimina se è uguale all'oggetto creato
     public static void verificaElementoEdElimina(HashSet<Integer> hashSet) {
-        // Creo l'oggetto dello stesso tipo inserito nell'HashSet e lo popolo
         Integer oggettoDaVerificare = 2;
 
-        // Scorro l'HashSet
         hashSet.removeIf(elemento -> elemento.equals(oggettoDaVerificare));
     }
 
-
     public static void svuotaHashSetEVerifica(HashSet<Integer> hashSet) {
-        // Svuoto l'HashSet
         hashSet.clear();
-
 
         if (hashSet.isEmpty()) {
             System.out.println("L'HashSet è vuoto.");
